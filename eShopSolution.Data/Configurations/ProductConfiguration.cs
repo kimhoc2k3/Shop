fcd107aaa49ea76295fc97100c56d418c3de95f6
Hoc Nguyen Kim <hq37na@gmail.com>
2023-03-12 22:45:00 +0700

    ..

---
+++ eShopSolution.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products"); 
+
+            builder.HasKey(x => x.Id); 
+
+            builder.Property(x=>x.price).IsRequired();
+
+            builder.Property(x=>x.OriginalPrice).IsRequired().HasDefaultValue(0);
+
+            builder.Property(x=>x.Stock).IsRequired().HasDefaultValue(0);
+
+            builder.Property(x=>x.ViewCount).IsRequired().HasDefaultValue(0);
+
+        }
+    }
+}
