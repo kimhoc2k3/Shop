82a12f1cbdd2e55deb59a3dc32f95cfc1f758b34
Hoc Nguyen Kim <hq37na@gmail.com>
2023-03-21 23:17:13 +0700

    login_AddCookie

---
+++ eShopSolution.AdminApp/Services/IUserApiClient.cs
@@ -0,0 +1,10 @@
+﻿using eShopSolution.ViewModel.System.Users;
+using System.Threading.Tasks;
+
+namespace eShopSolution.AdminApp.Services
+{
+    public interface IUserApiClient
+    {
+        Task<string> Authenticate(LoginRequest request);
+    }
+}
